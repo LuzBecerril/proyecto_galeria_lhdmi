4bad1f0cdf56d47cd04459b8babc5ea237cb174a
BereVelez <ab.jvelez@gmail.com>
2023-11-13 21:58:30 -0600

    Modificaciones pequeñas de espacios

--- js/contacto.js
+++ js/contacto.js
@@ -16,8 +16,7 @@ function validarNombre(){
     }//NaN value
 
     return true;
-}//validarNombre
-
+}//
 function telefono(){
     if ((txtNumber.value.length!==10)||(isNaN(txtNumber.value))){
         return false;
@@ -45,9 +44,8 @@ function validarCorreo(){
     }
     return true;
 }//validarCorreo
-
 document.getElementById('form')
-btn.addEventListener('submit',function(event){
+.addEventListener('submit',function(event){
     let isValid = true;
     event.preventDefault();
 
@@ -57,48 +55,45 @@ btn.addEventListener('submit',function(event){
     txtNumber.style.border="solid thin green";
     email.style.border="solid thin green";
     mensaje.style.border="solid thin green";
-
     if (! validarNombre()){
-        alertValidaciones.innerHTML="El campo <strong>Nombre Completo</strong> es requerido únicamente con letras</br>"
+        alertValidaciones.innerHTML+="El campo <strong>Nombre Completo</strong> es requerido únicamente con letras</br>"
         alertValidaciones.style.display="block"; 
         txtNombre.style.border="solid thin red";
         isValid = false;
     }//Nombre
 
     if (! telefono()){
-        alertValidaciones.innerHTML="El campo <strong>Teléfono</strong> es requerido a 10 cifras</br>"
+        alertValidaciones.innerHTML+="El campo <strong>Teléfono</strong> es requerido a 10 cifras</br>"
         alertValidaciones.style.display="block";
         txtNumber.style.border="solid thin red";
         isValid = false;
     }//telefono
-
     if (! validarCorreo()){
-        alertValidaciones.innerHTML="El campo <strong>E-mail</strong> es requerido en el siguiente formato:</br> tu_correo@gmail.com</br>"
+        alertValidaciones.innerHTML+="El campo <strong>E-mail</strong> es requerido en el siguiente formato:</br> tu_correo@gmail.com</br>"
         alertValidaciones.style.display="block"; 
         email.style.border="solid thin red";
         isValid = false;
     }//If txtNombre <3
 
     if (! validarMensaje()){
-        alertValidaciones.innerHTML="El campo <strong>Mensaje</strong> es requerido </br>"
+        alertValidaciones.innerHTML+="El campo <strong>Mensaje</strong> es requerido </br>"
         alertValidaciones.style.display="block"; 
         mensaje.style.border="solid thin red";
         isValid = false;
     }//If validarMansaje <3
-
     if (isValid){
-    btn.value = 'Enviando...';
-   const serviceID = 'default_service';
-   const templateID = 'template_i0ciivg';
-
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Enviado, gracias!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
-    }//isValid
-});//btn "enviar"
+        btn.value = 'Enviando...';
+       const serviceID = 'default_service';
+       const templateID = 'template_i0ciivg';
+    
+       emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+          btn.value = 'Send Email';
+          alert('Enviado, gracias!');
+        }, (err) => {
+          btn.value = 'Send Email';
+          alert(JSON.stringify(err));
+        });
+        }//isValid
+    });//btn "enviar"
 //Termina formulario de contacto
