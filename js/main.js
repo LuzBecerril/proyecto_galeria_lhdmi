df15510698fa8a019c908cd63ec98d489f7da823
CynthiaA <cynthiaesperanzaavilaperez@gmail.com>
2023-11-27 18:42:42 -0600

    Se linkeo Facebook

--- js/main.js
+++ js/main.js
@@ -115,7 +115,7 @@ div6.insertAdjacentHTML("afterbegin",`<div class="row col-8">
 <div class="row" id="redesimg" style="margin-bottom: 2px;">
 <ul class="nav nav-underline">
                     <li class="nav-item">
-                      <a class="nav-link active" aria-current="page" target="_blank" href="https://es-la.facebook.com/"><img  src="./src/img/facebook.png" alt="" style="height: 25px; width: auto;"></a>
+                      <a class="nav-link active" aria-current="page" target="_blank" href="./clon_facebook.html"><img  src="./src/img/facebook.png" alt="" style="height: 25px; width: auto;"></a>
                     </li>
                     <li class="nav-item">
                       <a class="nav-link active" aria-current="page" target="_blank" href="https://www.instagram.com/"><img src="./src/img/instagram.png" alt="" style="height: 25px; width: auto;"></a>
