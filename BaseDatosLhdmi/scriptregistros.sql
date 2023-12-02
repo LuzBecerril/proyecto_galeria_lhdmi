5e54465bfb814931b8f16822fa716724bbd1bd77
FerAldana <ma.fe.al.ll.00@gmail.com>
2023-12-02 01:58:49 -0600

    se cambiaron intos

--- BaseDatosLhdmi/scriptregistros.sql
+++ BaseDatosLhdmi/scriptregistros.sql
@@ -1,29 +1,29 @@
 USE lhdmidb;
 SELECT * FROM lhdmidb.usuarios; -- insert usuarios
 
-INSERT INTO lhdmidb.usuarios (usuario, correo, contraseña, registrof, tipo, foto, direccion) 
+INSERT INTO lhdmidb.usuarios (nombre, correo, contrasena, registrof, tipo, foto, direccion) 
 VALUES ('Akary Larios', 'akary.larios@gmail.com', 'Akary_01', '2023-11-23', 'usuario_mortal', 'https://drive.google.com/file/d/1hI2ROV7aQFNq19UA4adFVwy_bHnN3yIi/view?usp=sharing', 'Colima, colima, col. frutería, calle moras #10 , c.p 28100');
-INSERT INTO lhdmidb.usuarios (usuario, correo, contraseña, registrof, tipo, foto, direccion) 
+INSERT INTO lhdmidb.usuarios (nombre, correo, contrasena, registrof, tipo, foto, direccion) 
 VALUES ('Dánae Valencia', 'danae.valen96@gmail.com', 'Danae_01', '2023-11-23', 'usuario_mortal', 'https://drive.google.com/file/d/1hI2ROV7aQFNq19UA4adFVwy_bHnN3yIi/view?usp=sharing', 'Jalisco,Zapopan , col. frutería, calle moras #10 , c.p 28100');
-INSERT INTO lhdmidb.usuarios (usuario, correo, contraseña, registrof, tipo, foto, direccion) 
+INSERT INTO lhdmidb.usuarios (nombre, correo, contrasena, registrof, tipo, foto, direccion) 
 VALUES ('Alicia Perez', 'aliciapbarreda@gmail.com', 'Alicia_01', '2023-11-23', 'usuario_mortal', 'https://drive.google.com/file/d/1Kue9SJNLh0bAIKDQZA7R4Zj0r0StoP3N/view?usp=sharing', 'CDMX, Verónica Anzures, col. frutería, calle moras #10 , c.p 28100');
 
-INSERT INTO lhdmidb.usuarios (usuario, correo, contraseña, registrof, tipo, foto, direccion) 
+INSERT INTO lhdmidb.usuarios (nombre, correo, contrasena, registrof, tipo, foto, direccion) 
 VALUES ('Zuri Santos', 'zurisadaisantospadilla@gmail.com', 'Sant0s_8', '2023-11-23', 'usuario_mortal', 'https://drive.google.com/file/d/1FsWh_8WtostXlrE86oDbov9zGz_6inHP/view?usp=sharing', 'Estado de México, Ecatepec, col. frutería, calle moras #10 , c.p 28100');
 
-INSERT INTO lhdmidb.usuarios (usuario, correo, contraseña, registrof, tipo, foto, direccion) 
+INSERT INTO lhdmidb.usuarios (nombre, correo, contrasena, registrof, tipo, foto, direccion) 
 VALUES ('Berez Vélez', 'ab.jvelez@gmail.com', 'BerezVelez_01', '2023-11-23', 'usuario_mortal', 'https://drive.google.com/file/d/1keCnB3JHwmPO9e3tp1B0Ti_53R__hu17/view?usp=sharing', 'CDMX, MiguelHidalgo, col. frutería, calle moras #10 , c.p 28100');
 
-INSERT INTO lhdmidb.usuarios (usuario, correo, contraseña, registrof, tipo, foto, direccion) 
+INSERT INTO lhdmidb.usuarios (nombre, correo, contrasena, registrof, tipo, foto, direccion) 
 VALUES ('Fernanda Aldana', 'ma.fe.al.ll.00@gmail.com', 'Fernanda_01', '2023-11-23', 'usuario_mortal', 'https://drive.google.com/file/d/1jBH8It2p5XZBoN3cEqSQtofKS77tmKIi/view?usp=sharing', 'Jalisco, Zapotlán el Grande, col. frutería, calle moras #10 , c.p 28100');
 
-INSERT INTO lhdmidb.usuarios (usuario, correo, contraseña, registrof, tipo, foto, direccion) 
+INSERT INTO lhdmidb.usuarios (nombre, correo, contrasena, registrof, tipo, foto, direccion) 
 VALUES ('Luz Becerril', 'luzbecerril055@gmail.com', 'Luzbp_01', '2023-11-23', 'usuario_mortal', 'https://drive.google.com/file/d/1Wc9ypkeKY7Vd2zjlr6pfAY3q5JJif2Pb/view?usp=sharing', 'Jalisco, colima, col. frutería, calle moras #10 , c.p 28100');
 
-INSERT INTO lhdmidb.usuarios (usuario, correo, contraseña, registrof, tipo, foto, direccion) 
+INSERT INTO lhdmidb.usuarios (nombre, correo, contrasena, registrof, tipo, foto, direccion) 
 VALUES ('Galeria Hijas Izquierdo', 'hijasmariaizquierdogaleria@gmail.com', 'MariaDerecho_01', '2023-11-23', 'administrador', 'https://drive.google.com/file/d/1ksZTMC5cwfzN5fL1Zj08i9sGGNSAEAYi/view?usp=sharing', 'CDMX, MiguelHidalgo, col. Polanco, Enrique Ibsen 32-A , c.p 11560');
 
-INSERT INTO lhdmidb.usuarios (usuario, correo, contraseña, registrof, tipo, foto, direccion) 
+INSERT INTO lhdmidb.usuarios (nombre, correo, contrasena, registrof, tipo, foto, direccion) 
 VALUES ('Cynthia Avila', 'qfbcynthiaavi@gmail.com', 'C&n2023 ', '2023-11-23', 'usuario_mortal', 'https://drive.google.com/file/d/15pmUF6hQc0D4dZ_uDSjnIvvJm4SdH1wo/view?usp=sharing', 'Jalisco, Tala, col. El Refugio, calle México #100 , c.p 20100');
 
 -- fin de insert usuarios
