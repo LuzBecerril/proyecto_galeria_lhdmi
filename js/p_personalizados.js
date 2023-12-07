1ff88e84f8256b253c92517483441a82d69ce2cb
Zuri <zurisadaisantospadilla@gmail.com>
2023-12-06 22:52:37 -0600

    se corrigió avance de personalizables

--- js/p_personalizados.js
+++ js/p_personalizados.js
@@ -1,73 +1,82 @@
-//Empieza formulario de personalizables
-let title = document.getElementById("title");
-let autor = document.getElementById("autor");
-let description = document.getElementById("description");
-let section = document.getElementById("section");
-let section1 = document.getElementById("section");
-let btnpublicar =document.getElementById("btnPublicar");
-let img = document.getElementById("inputFile")
-
+let nombre= document.getElementById("nombre");
+let correo= document.getElementById("correo");
+let titulo = document.getElementById("titulo");
+let autora = document.getElementById("autora");
+let medida = document.getElementById("medida");
+let descripcion = document.getElementById("descripcion");
+let btnSolicitar =document.getElementById("btnSolicitar");
+let img = document.getElementById("inputFile");
 let cardbody = document.getElementById("CardNueva");
-
 let datos = new Array();
+let fileImage = document.getElementById("fileImage");
+let btnFake = document.getElementById("btnFake");
+let imageFile = document.getElementById("imageFile");
 
-let fileImage = document.getElementById('fileImage');
-let btnFake = document.getElementById('btnFake');
-let imageFile = document.getElementById('imageFile');
 
-function tituloobra(){
-  let titulo = document.getElementById("title").value;
-  document.getElementById("TituloObrapreview").innerHTML = "<strong>" + titulo + "</strong>";
-  if( !(/^[1-9]\d*$/.test(txtNumber.value))){ 
-    return false;
+function nombretxt(){
+  let nombre = document.getElementById("nombre").value;
+  document.getElementById("nombrePreview").innerHTML = "Solicitante: "+ "<strong>" + nombre + "</strong>";
 }
-return true;
+function correotxt(){
+  let correo = document.getElementById("correo").value;
+  document.getElementById("correoPreview").innerHTML = "Correo: "+ "<strong>" + correo + "</strong>";
+}
+function titulotxt(){
+    let titulo = document.getElementById("titulo").value;
+    document.getElementById("tituloPreview").innerHTML = "Título: "+ "<strong>" + titulo + "</strong>";
+  }
+  function autoratxt(){
+    let autora = document.getElementById("autora").value;
+    document.getElementById("autoraPreview").innerHTML = "Autora: "+ "<strong>" + autora + "</strong>";
   
-}//función autora
+        if (autora.value==1){
+          document.getElementById("seccionColor").className = "autora1";
+          document.getElementById("thefront").className = "thefrontT";
+          document.getElementById("theback").className = "thebackT";
+          document.getElementById("btnCarrito").className = "btn btn-light-opacity-25 btn btn-outline-warning";
+          document.getElementById("btnCarrito").innerHTML = `<i class="bi bi-cart-fill"></i>`;
+        }
+        if (section.value==2){
+          document.getElementById("seccionColor").className = "autora2";
+          document.getElementById("thefront").className = "thefrontS";
+          document.getElementById("theback").className = "thebackS";
+          document.getElementById("btnCarrito").className = "btn btn-light-opacity-25 btn btn-outline-warning";
+          document.getElementById("btnCarrito").innerHTML = `<i class="bi bi-cart-fill"></i>`;
+        }
+    }
+      
 
-function autortxt(){
-  let autora = document.getElementById("autor").value;
-  document.getElementById("Autorapreview").innerHTML =autora;
-  if( !(/^[1-9]\d*$/.test(txtNumber.value))){ 
-    return false;
-}
-return true;
-}//funcion 
-function descrtxt(){
-  let descripcion = document.getElementById("description").value;
-  document.getElementById("descripreview").innerHTML = descripcion;
-  if( !(/^[1-9]\d*$/.test(txtNumber.value))){ 
-    return false;
-}
-return true;
-}
-function preciotxt(){
-  let precio = document.getElementById("precio").value;
-  document.getElementById("preciopreview").innerHTML = "Precio: <strong>$ </strong>"+ "<strong>" + precio + "</strong>";
-  if( !(/^[1-9]\d*$/.test(txtNumber.value))){ 
-    return false;
-}
-return true;
-}
+  function medidatxt(){
+    let medida = document.getElementById("medida").value;
+    document.getElementById("medidaPreview").innerHTML = "Medidas: "+ "<strong>" + medida + "</strong>";
+    
+  
+        if (medida.value==1){
+          document.getElementById("seccionColor").className = "medida1";
+          document.getElementById("thefront").className = "thefrontT";
+          document.getElementById("theback").className = "thebackT";
+          document.getElementById("btnCarrito").className = "btn btn-light-opacity-25 btn btn-outline-warning";
+          document.getElementById("btnCarrito").innerHTML = `<i class="bi bi-cart-fill"></i>`;
+        }
+        if (medida.value==2){
+          document.getElementById("seccionColor").className = "medida2";
+          document.getElementById("thefront").className = "thefrontS";
+          document.getElementById("theback").className = "thebackS";
+          document.getElementById("btnCarrito").className = "btn btn-light-opacity-25 btn btn-outline-warning";
+          document.getElementById("btnCarrito").innerHTML = `<i class="bi bi-cart-fill"></i>`;
+        }
+        if (medida.value==2){
+            document.getElementById("seccionColor").className = "medida3";
+            document.getElementById("thefront").className = "thefrontS";
+            document.getElementById("theback").className = "thebackS";
+            document.getElementById("btnCarrito").className = "btn btn-light-opacity-25 btn btn-outline-warning";
+            document.getElementById("btnCarrito").innerHTML = `<i class="bi bi-cart-fill"></i>`;
+          }
+  }
 
-function seccionseleccion(){
-  if (section.value==1){
-    document.getElementById("seccionColor").className = "seccionT";
-    document.getElementById("thefront").className = "thefrontT";
-    document.getElementById("theback").className = "thebackT";
-  }
-  if (section.value==2){
-    document.getElementById("seccionColor").className = "seccionS";
-    document.getElementById("thefront").className = "thefrontS";
-    document.getElementById("theback").className = "thebackS";
-  }
-  if (section.value==3){
-    document.getElementById("seccionColor").className = "seccionP";
-    document.getElementById("thefront").className = "thefrontP";
-    document.getElementById("theback").className = "thebackP";
-    document.getElementById("btnCarrito").className = "btn btn-primary-opacity-25 btn btn-outline-primary";
-    document.getElementById("btnCarrito").innerHTML = `<i class="bi bi-pen-fill"></i>`;
-  }
+function descripciontxt(){
+  let descripcion = document.getElementById("descripcion").value;
+  document.getElementById("descripcionPreview").innerHTML = "Descripción: "+ descripcion;
 }
 
 btnFake.addEventListener('click', function(){
@@ -94,36 +103,43 @@ fileImage.addEventListener('change', function(){
           }// file
     }// previewFile 
 
-function validarautor(){
-  if( autor.value == null || autor.value == 0 ||(! /^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(autor.value))) { 
+
+function validarnombre(){
+  if( nombre.value == null || nombre.value == 0 ||(! /^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(autor.value))) { 
     return false;
   }
     return true;
-  }//validar autor
-function validartitulo(){
-    if( title.value == null || title.value == 0) { 
+  }//validar nombre
+function validarcorreo(){
+    if( correo.value == null || correo.value == 0) { 
       return false;
     }
       return true;
-    }//validar autor
-function validardecripcion(){
-  if (description.value==0){
+    }//validar correo
+function validartitulo(){
+  if (titulo.value==0){
     return false;
   }//mensaje 0
     return true;
 }//validar descripcion
-function validarprecio(){
-  if (precio.value == 0){
-    return false;
-  }//precio
-    return true;
-}//validar precio
-function validarseccion(){
-  if(section.value == ""){
+function validarautora(){
+    if( autora.value == null || autora.value == 0 ||(! /^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(autora.value))) { 
+      return false;
+    }
+      return true;
+}//validarautora
+function validarmedida(){
+  if(medida.value == ""){
     return false;
   }
   return true;
-}
+}//validar medida
+function validardecripcion(){
+    if (descripcion.value==0){
+      return false;
+    }//mensaje 0
+      return true;
+  }//validar descripcion
 function validarimg(){
   if(img.value == ""){
     return false;
